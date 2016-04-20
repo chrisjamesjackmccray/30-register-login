decd3d597f085fa9445837440f6eb866159d09ee
James McCray <chrisjamesjackmccray@gmail.com>
2016-04-20 14:39:00 -0500

    C

--- app/modules/user/config.js
+++ app/modules/user/config.js
@@ -5,6 +5,25 @@
     3. register which goes to /register
 */
 function config($stateProvider) {
+  .$stateProvider
+  .state('user', {
+    url: '/',
+    controller: 'profileController as profileCtrl',
+    template: require('./view.html')
+  })
+
+  .state('user', {
+    url: '/login',
+    controller: 'loginController as loginCtrl',
+    template: require('./view.html')
+  })
+
+  .state('user', {
+    url: '/register',
+    controller: 'registerController as registerCtrl',
+    template: require('./view.html')
+  });
+
 }
 
 export default config;

7a9e7633f1141b9b35054a21b0fec9ef662773bb
James McCray <chrisjamesjackmccray@gmail.com>
2016-04-21 10:55:24 -0500

    C

---
+++ app/modules/map/controller.js
@@ -0,0 +1,19 @@
+class MapController {
+  constructor() {
+    this.addresses = [
+      {
+        address: "8 Flag Rd",
+        city: "Little Rock",
+        State: "Arkansas"
+      }
+
+    ]
+
+  }
+
+  clickMarker() {
+
+  }
+}
+
+export default MapController
